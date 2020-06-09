4fb14034350570a302b99854f9952f3d76fdd783
Frank Mai <frank@rancher.com>
2020-06-09 11:17:52 +0800

    fix: failed to package images

--- windows/scripts/package.ps1
+++ windows/scripts/package.ps1
@@ -15,7 +15,7 @@ if ($ONLY_PACKAGE_IMAGES) {
     $targets = Get-ChildItem -Path $SRC_PATH -Directory -Exclude $excludes
 }
 
-targets | Sort-Object -Descending | ForEach-Object {
+$targets | Sort-Object -Descending | ForEach-Object {
     $dirName = $_.Name
     Invoke-Script -File "$SRC_PATH\$dirName\scripts\ci.ps1"
 }
